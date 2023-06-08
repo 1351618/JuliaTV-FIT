f961839a465ae2ed121870e440cf91248a3b2c41
sergei <1351618@gmail.com>
2023-06-08 18:15:27 +0300

    footer

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import WhatsappIcon from '../images/whatsapp_icon.png';
+import PhoneIcon from '../images/phone_icon.png';
+import TelegramIcon from '../images/telegram_icon.png';
+import InstagramIcon from '../images/instagram_icon.png';
+
 
 class Footer extends React.Component {
     render() {
         return (
-            <div>000000000000000000000</div>
+            <div className="footer">
+                <a href="tel:+1234567890"><img src={PhoneIcon} alt="" /></a>
+                <a href="/"><img src={TelegramIcon} alt="" /></a>
+                <a href="https://instagram.com/julia____tv?igshid=NTc4MTIwNjQ2YQ=="><img src={InstagramIcon} alt="" /></a>
+                <a href="/"><img src={WhatsappIcon} alt="" /></a>
+            </div>
         )
     }
 }
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,7 +3,9 @@ import React from "react";
 class Main extends React.Component {
     render() {
         return (
-            <div>000000000000000000000</div>
+            <div>
+                "text"
+            </div>
         )
     }
 }
